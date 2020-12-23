59c89faf862d66ddb6fea99ad6b97ca9db7c2b65
mak art <makart@live.co.uk>
2020-12-23 09:48:04 -0800

    MORE CLEAN UP , REMOVING UNUSED FILES AND IMPORTS

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,8 @@
 import React from "react";
 import { render } from "react-dom";
-//import "./styles/main.scss";
+
 import "./styles/newstyles/main_new.scss";
-//import "./mediauploader/styles/main.scss";
-//components
 
-//import
-
-//import App from "./components/app2";
 import { App } from "./new_components/app_new";
 
 //import { App } from "./mediauploader/app";
